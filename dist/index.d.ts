b279499bb38f0154566dd6de7b0ed2562fe425e9
nivinjoseph <nivinjoseph@outlook.com>
2018-10-04 08:00:53 -0400

    preparing to publish new version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import "@nivinjoseph/n-ext";
 export declare abstract class ConfigurationManager {
+    static readonly configObject: object;
     private constructor();
     static getConfig<T>(key: string): T;
 }
